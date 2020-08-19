f4479c61d8db8cc42db6ee02ade51dd52d829b34
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-08-18 21:12:30 -0300

    feat: Creating the java api / backend for frontend

---
+++ src/main/java/com/luizfoli/apiestantevirtual/apiestantevirtual/ApiestantevirtualApplication.java
@@ -0,0 +1,13 @@
+package com.luizfoli.apiestantevirtual.apiestantevirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiestantevirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiestantevirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizfoli/apiestantevirtual/apiestantevirtual/ApiestantevirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizfoli.apiestantevirtual.apiestantevirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiestantevirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
