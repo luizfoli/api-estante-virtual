644c25e3060d2d453cdcff90d293ee4a15b754dc
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-08-18 22:00:56 -0300

    feat: creating the class model Book

---
+++ src/main/java/com/luizfoli/apiestantevirtual/models/Book.java
@@ -0,0 +1,68 @@
+package com.luizfoli.apiestantevirtual.models;
+
+public class Book {
+
+    private String id;
+    private String title;
+    private String author;
+    private String publisher;
+    private String publishedDate;
+    private String description;
+    private int pageCount;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+}
