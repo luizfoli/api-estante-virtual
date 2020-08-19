06e7b7d7e0da366a6f63563b00f774e21d2308ce
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-08-18 22:01:50 -0300

    feat: changes

--- src/main/java/com/luizfoli/apiestantevirtual/controllers/BooksController.java
+++ src/main/java/com/luizfoli/apiestantevirtual/controllers/BooksController.java
@@ -1,16 +1,24 @@
 package com.luizfoli.apiestantevirtual.controllers;
 
+import com.luizfoli.apiestantevirtual.utils.RequestMaker;
+
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/book")
 public class BooksController {
 
+    private RequestMaker requestMaker = new RequestMaker();
 
     @GetMapping
-    public String get() {
-        return "Hello World!";
+    public JSONObject get(@RequestParam String name) {
+        name = name.replaceAll(" ", "+");
+        JSONObject retorno = requestMaker.get("https://www.googleapis.com/books/v1/volumes?q=" + name);
+        System.out.println(retorno);
+        return retorno;
     }
 }
