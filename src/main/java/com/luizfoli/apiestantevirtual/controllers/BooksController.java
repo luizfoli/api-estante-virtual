d4c5b77c5e5d3be5a4f8421225ce1766a7b0c194
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-08-18 22:27:26 -0300

    feat: Finalizando ajustes para receber o nome/titulo do livro por parametro e enviar para api do google

--- src/main/java/com/luizfoli/apiestantevirtual/controllers/BooksController.java
+++ src/main/java/com/luizfoli/apiestantevirtual/controllers/BooksController.java
@@ -1,5 +1,7 @@
 package com.luizfoli.apiestantevirtual.controllers;
 
+import com.luizfoli.apiestantevirtual.models.Book;
+import com.luizfoli.apiestantevirtual.services.BookService;
 import com.luizfoli.apiestantevirtual.utils.RequestMaker;
 
 import org.json.JSONObject;
@@ -13,12 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class BooksController {
 
     private RequestMaker requestMaker = new RequestMaker();
+    private BookService bookService = new BookService();
 
     @GetMapping
-    public JSONObject get(@RequestParam String name) {
+    public Book get(@RequestParam String name) {
+        System.out.println(name);
         name = name.replaceAll(" ", "+");
+        System.out.println(name);
+        System.out.println("https://www.googleapis.com/books/v1/volumes?q=" + name);
         JSONObject retorno = requestMaker.get("https://www.googleapis.com/books/v1/volumes?q=" + name);
-        System.out.println(retorno);
-        return retorno;
+        Book book = bookService.convertJsonToBook(retorno);
+        return book;
     }
 }
--- src/main/java/com/luizfoli/apiestantevirtual/services/BookService.java
+++ src/main/java/com/luizfoli/apiestantevirtual/services/BookService.java
@@ -8,6 +8,15 @@ public class BookService {
 
     public Book convertJsonToBook(JSONObject json) {
         Book book = new Book();
+
+        JSONObject volumeInfo = json.getJSONArray("items").getJSONObject(0).getJSONObject("volumeInfo");
+
+        book.setTitle(volumeInfo.getString("title"));
+        book.setDescription(volumeInfo.getString("description"));
+        //book.setPublisher(volumeInfo.getString("publisher"));
+        //book.setPublishedDate(volumeInfo.getString("publishedDate"));
+        book.setPageCount(volumeInfo.getInt("pageCount"));
+
         return book;
     }
 
