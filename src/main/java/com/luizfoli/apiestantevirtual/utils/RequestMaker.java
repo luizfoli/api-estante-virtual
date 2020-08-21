966d71b29dba5fe46de60daa193351b221ce8569
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-08-20 22:12:05 -0300

    feat: Finish utils for request maker

--- src/main/java/com/luizfoli/apiestantevirtual/utils/RequestMaker.java
+++ src/main/java/com/luizfoli/apiestantevirtual/utils/RequestMaker.java
@@ -1,34 +1,29 @@
 package com.luizfoli.apiestantevirtual.utils;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.json.JSONObject;
 import org.json.JSONTokener;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RequestMaker {
 
     public JSONObject get(String urlPath) {
-
         try {
-            URL url = new URL(urlPath);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            HttpURLConnection con = (HttpURLConnection) new URL(urlPath).openConnection();
             con.setRequestMethod("GET");
             con.connect();
-
             BufferedReader response = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            JSONTokener tokener = new JSONTokener(response);
-            JSONObject json = new JSONObject(tokener);
-
-            return json;
-
+            return new JSONObject(new JSONTokener(response));
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println(e.getStackTrace());
             return null;
         }
-
     }
-
 };
