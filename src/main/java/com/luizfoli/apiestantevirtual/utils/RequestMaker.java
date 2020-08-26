7c14a5d4b1d74508bc8626dbdc48da14b9ee130b
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-08-25 22:22:49 -0300

    feat: RequestMaker - Ajustando

--- src/main/java/com/luizfoli/apiestantevirtual/utils/RequestMaker.java
+++ src/main/java/com/luizfoli/apiestantevirtual/utils/RequestMaker.java
@@ -1,10 +1,8 @@
 package com.luizfoli.apiestantevirtual.utils;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.json.JSONObject;
