53b7e1db7cc8ab41fa2d93b587f9fa413272ebb8
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-08-25 22:23:29 -0300

    feat: VolumeGoogleBookApi - Criando model classe para resposta da api do Google

--- src/main/java/com/luizfoli/apiestantevirtual/adapters/googlebooksapi/models/VolumeGoogleBookApi.java
+++ src/main/java/com/luizfoli/apiestantevirtual/adapters/googlebooksapi/models/VolumeGoogleBookApi.java
@@ -1,20 +1,155 @@
 package com.luizfoli.apiestantevirtual.adapters.googlebooksapi.models;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class VolumeGoogleBookApi {
 
-    public String id;
-    public String title;
-    public String[] authors;
-    public String publisher;
-    public String publishedDate;
-    public String description;
-    public int pageCount;
-    public int averageRating;
-    public int ratingCount;
-    public String imageLink;
+    private String id;
+    private String title;
+    private String[] authors;
+    private String publisher;
+    private String publishedDate;
+    private String description;
+    private int pageCount;
+    private int averageRating;
+    private int ratingCount;
+    private String imageLink;
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the authors
+     */
+    public String[] getAuthors() {
+        return authors;
+    }
+
+    /**
+     * @param authors the authors to set
+     */
+    public void setAuthors(String[] authors) {
+        this.authors = authors;
+    }
+
+    /**
+     * @return the publisher
+     */
+    public String getPublisher() {
+        return publisher;
+    }
+
+    /**
+     * @param publisher the publisher to set
+     */
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    /**
+     * @return the publishedDate
+     */
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    /**
+     * @param publishedDate the publishedDate to set
+     */
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the pageCount
+     */
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    /**
+     * @param pageCount the pageCount to set
+     */
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    /**
+     * @return the averageRating
+     */
+    public int getAverageRating() {
+        return averageRating;
+    }
+
+    /**
+     * @param averageRating the averageRating to set
+     */
+    public void setAverageRating(int averageRating) {
+        this.averageRating = averageRating;
+    }
+
+    /**
+     * @return the ratingCount
+     */
+    public int getRatingCount() {
+        return ratingCount;
+    }
+
+    /**
+     * @param ratingCount the ratingCount to set
+     */
+    public void setRatingCount(int ratingCount) {
+        this.ratingCount = ratingCount;
+    }
+
+    /**
+     * @return the imageLink
+     */
+    public String getImageLink() {
+        return imageLink;
+    }
+
+    /**
+     * @param imageLink the imageLink to set
+     */
+    public void setImageLink(String imageLink) {
+        this.imageLink = imageLink;
+    }
 }
